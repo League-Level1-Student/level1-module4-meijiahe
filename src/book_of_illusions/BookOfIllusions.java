61293f6da182ec3d64619ca84dafe59389edd096
William Kwak <league@wts-imac-11.attlocal.net>
2019-04-28 13:02:04 -0700

    typinggameandbookofillusion

--- src/book_of_illusions/BookOfIllusions.java
+++ src/book_of_illusions/BookOfIllusions.java
@@ -11,6 +11,7 @@ import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
  
 /** We’re going to make a slideshow of cool optical illusions. When the user clicks on an illusion, a new one will be loaded. **/
@@ -18,24 +19,38 @@ import javax.swing.JLabel;
 public class BookOfIllusions extends MouseAdapter {
 
 	// 1. Make a JFrame variable and initialize it using "new JFrame()"
-
+JFrame frame =new JFrame();
 	public void run() {
 		// 2. make the frame visible
+		frame.setVisible(true);
 		// 3. set the size of the frame
+		frame.setSize(200, 400);
 		// 4. find 2 images and save them to your project’s default package
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
+		String illusion1="illusion.png";
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
+		JLabel label;
 		// 7. use the "loadImage..." methods below to initialize your JLabel
+		label=loadImageFromComputer(illusion1);
 		// 8. add your JLabel to the frame
+		frame.add(label);
 		// 9. call the pack() method on the frame
+		frame.pack();
 		// 10. add a mouse listener to your frame (hint: use *this*)
+		frame.addMouseListener(this);
 	}
-
+	String illusion2="illusion1.jpg";
 	public void mousePressed(MouseEvent e) {
 		// 11. Print "clicked!" to the console when the mouse is pressed
+		System.out.println("clicked");
 		// 12. remove everything from the frame that was added earlier
+		frame.removeAll();
 		// 13. load a new image like before (this is more than one line of code)
+		JLabel label1;
+		label1=loadImageFromComputer(illusion2);
+		frame.add(label1);
 		// 14. pack the frame
+		frame.pack();
 	}
 
 	// [OPTIONAL] 15. goad your users with some annoying or witty pop-ups
--- src/simon_says/Typing_Game.java
+++ src/simon_says/Typing_Game.java
@@ -1,16 +1,40 @@
 package simon_says;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Date;
+import java.util.Random;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
-public class Typing_Game {
+public class Typing_Game implements KeyListener {
 public static void main(String[] args) {
-JFrame frame=new JFrame("Type or Die");	
-
-char currentLetter;
-
-currentLetter = generateRandomLetter();
+	Typing_Game typegame=new Typing_Game();
+	typegame.createUI();
 }
-static char generateRandomLetter() {
+JFrame frame=new JFrame("Type or Die");	
+static JLabel label=new JLabel();
+static JPanel panel=new JPanel();
+char currentLetter;
+public void createUI () {
+	frame.addKeyListener(this);
+	frame.setVisible(true);
+	frame.add(panel);
+	panel.add(label);
+	frame.pack();
+	
+
+	currentLetter = generateRandomLetter1();
+	label.setFont(label.getFont().deriveFont(28.0f));
+	label.setHorizontalAlignment(JLabel.CENTER);
+label.setText(currentLetter+"");
+}
+static char generateRandomLetter1() {
       Random r = new Random();
       return (char) (r.nextInt(26) + 'a');
 }
@@ -22,4 +46,36 @@ private static char generateRandomLetter() {
 	// TODO Auto-generated method stub
 	return 0;
 }
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void keyPressed(KeyEvent e) {
+	// TODO Auto-generated method stub
+if (e.getKeyChar()==currentLetter) {
+	System.out.println("correct");
+	panel.setBackground(Color.GREEN);
+}
+else {
+	panel.setBackground(Color.red);
+}
+}
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+Date timeAtStart = new Date();
+
+private void showTypingSpeed(int numberOfCorrectCharactersTyped) {
+      Date timeAtEnd = new Date();
+      long gameDuration = timeAtEnd.getTime() - timeAtStart.getTime();
+      long gameInSeconds = (gameDuration / 1000) % 60;
+      double charactersPerSecond = ((double) numberOfCorrectCharactersTyped / (double) gameInSeconds);
+      int charactersPerMinute = (int) (charactersPerSecond * 60);
+      JOptionPane.showMessageDialog(null, "Your typing speed is " + charactersPerMinute + " characters per minute.");
+}
+
 }
