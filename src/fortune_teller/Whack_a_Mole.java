051497905ceb90a3dac20a0041e6383fad4a1bb6
league <league@imac.attlocal.net>
2019-05-29 18:26:06 -0700

    whackamoleyetfinish6and7

--- src/fortune_teller/Whack_a_Mole.java
+++ src/fortune_teller/Whack_a_Mole.java
@@ -2,24 +2,45 @@ package fortune_teller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Whack_a_Mole implements ActionListener {
 JFrame frame = new JFrame();
 JPanel panel=new JPanel();
 JLabel label=new JLabel();
+int rr=0;
+int rl=0;
+Date nd=new Date();
+public static void main(String[] args) {
+Whack_a_Mole dang=new Whack_a_Mole();
+dang.run();
+dang.draw();
+}
 void draw() {
-	for (int i = 0;i<16;i++) {
+	Random r=new Random();
+	int ra=r.nextInt(15);
+	for (int i = 0;i<15;i++) {
 		JButton button=new JButton();
 		button.addActionListener(this);
+		panel.add(button);
+		if (i==ra) {
+			button.setText("Mole!");
+		}
 	}
 }
 void run() {
 	frame.setTitle("Whack a Button for frame and Glory");
+	frame.setVisible(true);
+	frame.add(panel);
+	JButton buttonm=new JButton();
+	buttonm.setText("Mole!");
 }
 void speak(String words) { 
     try { 
@@ -31,6 +52,23 @@ void speak(String words) {
 @Override
 public void actionPerformed(ActionEvent e) {
 	// TODO Auto-generated method stub
-	
+	JButton pressed=(JButton) e.getSource();
+	if (pressed.getText().equals("Mole!")) {
+		speak("You chose the correct button");
+		frame.dispose();
+		draw();
+		rl++;
+	}
+	rr++;
+	if (rr>10) {
+		endGame(nd,rl);
+	}
+}
+private void endGame(Date timeAtStart, int molesWhacked) {
+    Date timeAtEnd = new Date();
+    JOptionPane.showMessageDialog(null, "Your whack rate is "
+         + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
+         + " moles per second.");
 }
 }
+
