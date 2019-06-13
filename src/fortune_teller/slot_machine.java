0d041682809538bb8a7f664e7eaabbc86a2a6071
league <league@wts-imac-11.attlocal.net>
2019-06-12 17:34:37 -0700

    slotmachine

--- src/fortune_teller/slot_machine.java
+++ src/fortune_teller/slot_machine.java
@@ -14,53 +14,85 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class slot_machine implements ActionListener {
-JButton spin=new JButton();
-JFrame frame=new JFrame();
-JPanel panel=new JPanel();
-JLabel label=new JLabel();
-JLabel labela=new JLabel();
-JLabel labelb=new JLabel();
-void run() {
-	frame.setTitle("Slot Machine");
-	frame.setVisible(true);
-	panel.add(spin);
-	panel.add(label);
-	frame.add(panel);
-	frame.pack();
-	spin.addActionListener(this);
-	try {
-	createLabelImage("Cherries-512.jpg");
-	createLabelImage("Orange-512.jpg");
-	createLabelImage("Watermelon-512.jpg");
-	} catch (MalformedURLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	JButton spin = new JButton();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	JLabel labela = new JLabel();
+	JLabel labelb = new JLabel();
+
+	void run() {
+		frame.setTitle("Slot Machine");
+		frame.setVisible(true);
+		panel.add(spin);
+		frame.add(panel);
+		frame.pack();
+		spin.addActionListener(this);
+		try {
+			label=createLabelImage("Cherries-512.png");
+			labela=createLabelImage("Orange-512.png");
+			labelb=createLabelImage("Watermelon-512.png");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-}
-void spin() {
-Random r1=new Random();
-int num=r1.nextInt(3);
-if (num==0) {
-	
-}
-}
-private JLabel createLabelImage(String fileName) throws MalformedURLException{
-    URL imageURL = getClass().getResource(fileName);
-if (imageURL == null){
-	System.err.println("Could not find image " + fileName);
-	return new JLabel();
-}
-Icon icon = new ImageIcon(imageURL);
-JLabel imageLabel = new JLabel(icon);
-return imageLabel;
-}
-@Override
-public void actionPerformed(ActionEvent e) {
-	// TODO Auto-generated method stub
-	JButton pressed=(JButton) e.getSource();
-	if (spin==pressed) {
-		
+
+	void spin() {
+		panel.removeAll();
+		panel.add(spin);
+		Random r1 = new Random();
+		int num = r1.nextInt(3);
+		if (num == 0) {
+panel.add(label);
+		}
+		if (num==1) {
+			panel.add(labela);
+		}
+		if (num==2) {
+			panel.add(labelb);
+		}
+		int num1= r1.nextInt(3);
+		if (num1 == 0) {
+panel.add(label);
+		}
+		if (num1==1) {
+			panel.add(labela);
+		}
+		if (num1==2) {
+			panel.add(labelb);
+		}
+		int num2 = r1.nextInt(3);
+		if (num2 == 0) {
+panel.add(label);
+		}
+		if (num2==1) {
+			panel.add(labela);
+		}
+		if (num2==2) {
+			panel.add(labelb);
+		}
+		frame.pack();
 	}
-}
+
+	private JLabel createLabelImage(String fileName) throws MalformedURLException {
+		URL imageURL = getClass().getResource(fileName);
+		if (imageURL == null) {
+			System.err.println("Could not find image " + fileName);
+			return new JLabel();
+		}
+		Icon icon = new ImageIcon(imageURL);
+		JLabel imageLabel = new JLabel(icon);
+		return imageLabel;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton pressed = (JButton) e.getSource();
+		if (spin == pressed) {
+spin();
+		}
+	}
 
 }
---
+++ src/fortune_teller/slotmachinerunner.java
@@ -0,0 +1,8 @@
+package fortune_teller;
+
+public class slotmachinerunner {
+public static void main(String[] args) {
+	slot_machine s=new slot_machine();
+	s.run();
+}
+}
